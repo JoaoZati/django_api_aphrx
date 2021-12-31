2045135f2e6f96d971990fff78a9d0c1e895fd3b
JoaoZati <joaozati@gmail.com>
2021-12-31 13:33:51 -0300

    Create first django rest framework view

--- src/drf_api/core/views.py
+++ src/drf_api/core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
-def test_view(request):
+#third part frameworks
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+        'name': 'john',
+        'age': '23',
+        }
+
+        return Response(data)
+
+""" def test_view(request):
     data = {
         'name': 'john',
         'age': '23',
     }
 
-    return JsonResponse(data) 
+    return JsonResponse(data)  """
